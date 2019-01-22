74f55eb3312b84ce77d79afb86b71f0034034df3
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-22 14:38:52 -0500

    03 Simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,43 @@
-#write your code here
+def echo str
+    str
+end
+
+def shout str
+    str.upcase
+end
+
+def repeat str, count=1
+    repeated = str + ' ' + str
+    while count > 2
+        repeated += ' ' + str
+        count -= 1
+    end
+    repeated
+end
+
+def start_of_word word, length
+    word[0, length]
+end
+
+def first_word sentence
+    arr = sentence.split(' ')
+    arr[0]
+end
+
+def titleize str
+    if !str.include? ' '
+        return str.capitalize
+    else 
+        arr = str.split(' ')
+        title = []
+        little_words = ['and', 'the', 'over']
+        arr.each do |word|
+            if not little_words.include? word or title.length == 0
+                title.push word.capitalize
+            else
+                title.push word
+            end
+        end
+        return title.join(' ')
+    end
+end
