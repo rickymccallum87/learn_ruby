82ba0850ba081e33a9074d1a5e0a7b8cca96962b
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-23 10:55:47 -0500

    05 Book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+    attr_reader :title
+
+    def title= new_title
+        if !new_title.include? ' '
+            @title = new_title.capitalize
+        else
+            new_title_words = new_title.split(' ')
+            title_words = []
+            conjunctions = ['and']
+            prepositions = ['in', 'of']
+            articles = ['a', 'the', 'an']
+            new_title_words.each do |word|
+                if (conjunctions.include? word or
+                            prepositions.include? word or
+                            articles.include? word) and
+                            title_words.length > 0
+                    title_words.push word
+                else
+                    title_words.push word.capitalize
+                end
+            end
+            @title = title_words.join(' ')
+        end
+    end
 end
